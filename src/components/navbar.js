ec8c1407d2cb29161f880ad65e15577ee4ae684c
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-01-18 17:20:47 +0530

    initial

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import logo from '../logo-final.png'
+import React from "react";
+import logo from "../logo-final.png";
 export default function navbar() {
-    return (
-        <nav className="navbar">
-        <div className="content">
-          <div className="logo"><img src={logo}></img><a href="#"></a>
+  return (
+    <nav className="navbar">
+      <div className="content">
+        <div className="logo">
+          <img src={logo}></img>
+          <a href="#"></a>
           <h2>
-          <span>H</span>ACK
-          <span>C</span>LUB
-          <span>S</span>VIT
+            <span>H</span>ACK
+            <span>C</span>LUB
+            <span>S</span>VIT
           </h2>
-          
+        </div>
+        <ul className="menu-list">
+          <div className="icon cancel-btn">
+            <i className="fas fa-times"> </i>
           </div>
-          <ul className="menu-list">
-            <div className="icon cancel-btn">
-              <i className="fas fa-times"> </i>
-            </div>
-            <li><a href="#about">About</a></li>
-            <li><a href="#">Gallery</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#">Team</a></li>
-          </ul>
-          <div className="icon menu-btn">
-            <i className="fas fa-bars"> </i>
-          </div>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#">Gallery</a>
+          </li>
+          <li>
+            <a href="#events">Events</a>
+          </li>
+          <li>
+            <a href="#">Team</a>
+          </li>
+        </ul>
+        <div className="icon menu-btn" >
+          <i className="fas fa-bars"> </i>
+        </div>
       </div>
-      
-
-      </nav>
-    )
+    </nav>
+  );
 }
